1aa50710f305ecf88547cf1caa5ba32df616b6c8
Yuri Bekmetov <bekm.yu@yandex.ru>
2023-02-02 15:53:34 +0500

    styles changed

--- src/app/header/ToggleLocale.tsx
+++ src/app/header/ToggleLocale.tsx
@@ -5,8 +5,10 @@ import * as Icon from "react-bootstrap-icons";
 export default function ToggleLocale() {
     const { locale, toggleLocale } = useContext(LocaleContext);
     return (
-      <button onClick={() => toggleLocale()}>
-        {locale === "en" ? <Icon.GlobeAmericas /> : <Icon.GlobeCentralSouthAsia />}
-      </button>
+      <div className="toggle-locale">
+        <button onClick={() => toggleLocale()}>
+          {locale === "en" ? <span><Icon.GlobeAmericas /> en</span> : <span><Icon.GlobeCentralSouthAsia /> ru</span>}
+        </button>
+      </div>
   )
 }
